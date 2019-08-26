7b8800a4c488172537626e37065c2da2f5479661
Prayush Dawda <35144226+iamprayush@users.noreply.github.com>
2019-08-27 01:00:09 +0530

    Create cpp_template.cpp

---
+++ cpp_template.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define pb push_back
+#define ll long long
+#define lli long long int
+#define li long int
+#define ld long double
+#define vi vector<int>
+#define vlli vector<lli>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define mp(a, b) make_pair(a, b)
+#define all(arr) arr.begin(), arr.end()
+using namespace std;
+const lli mod = 1e9 + 7;
+const lli mod_ = 998244353;
+const int INF = 1000000000;
+const lli INF64 = (lli)1E18;
+
+int main() {
+	// ifstream cin("input.in");
+	// ofstream cout("output.out");
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	return 0;
+}
